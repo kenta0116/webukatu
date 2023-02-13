cbe1c2077506e93ff8f349b5a848ace4d10c8c01
kenta0116 <23292439+kenta0116@users.noreply.github.com>
2023-02-13 21:35:25 +0900

    2/13分

--- sample/index.php
+++ sample/index.php
@@ -6,13 +6,14 @@ ini_set('display_errors', 'On'); //画面にエラーを表示させるか
 //1.post送信されていた場合
 if(!empty($_POST)) {
 
+  //エラーメッセージを定数に設定
   define('MSG01','入力必須です');
   define('MSG02','Emailの形式で入力してください');
   define('MSG03','パスワード（再入力）が合っていません');
   define('MSG04','半角英数字のみご利用いただけます');
   define('MSG05','6文字以上で入力してください');
 
-  $err_flg = false;
+  //配列$err_msgを用意
   $err_msg = array();
 
   //2.フォームが入力されていない場合
@@ -24,10 +25,11 @@ if(!empty($_POST)) {
     $err_msg['pass'] = MSG01;
   }
 
-  if(empty[$_POST['pass_retype']]) {
+  if(empty($_POST['pass_retype'])) {
     $err_msg['pass_retype'] = MSG01;
   }
 
+  //変数にユーザー情報を代入
   if(empty($err_msg)) {
     $email = $_POST['email'];
     $pass = $_POST['pass'];
@@ -51,10 +53,10 @@ if(!empty($_POST)) {
 
       }elseif(mb_strlen($pass) < 6 ) {
         //6.パスワードとパスワード再入力が6文字以上でない場合
-        $err_msg['pass'] = MSG05:
+        $err_msg['pass'] = MSG05;
       }
 
-      if(empty($err_msg)) header("Location:mypage.php")
+      if(empty($err_msg)) header("Location:mypage.php");
     }
   }
 }
@@ -80,6 +82,16 @@ if(!empty($_POST)) {
         overflow: hidden;
       }
       input[type="text"] {
+        color: #545454;
+        height: 60px;
+        width: 100%;
+        padding: 5px 10px;
+        font-size: 16px;
+        display: block;
+        margin-bottom: 10px;
+        box-sizing: border-box;
+      }
+      input[type="password"] {
         color: #545454;
         height: 60px;
         width: 100%;
@@ -117,11 +129,11 @@ if(!empty($_POST)) {
     <h1>ユーザー登録</h1>
     <form action="" method="post">
       <span class="err_msg"><?php if(!empty($err_msg['email'])) echo $err_msg['email']; ?></span>
-      <input type="text" name="email" placeholder="email">
+      <input type="text" name="email" placeholder="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email'];?>">
       <span class="err_msg"><?php if(!empty($err_msg['pass'])) echo $err_msg['pass']; ?></span>
-      <input type="text" name="pass" placeholder="パスワード">
+      <input type="password" name="pass" placeholder="パスワード" value="<?php if(!empty($_POST['pass'])) echo $_POST['pass'];?>">
       <span class="err_msg"><?php if(!empty($err_msg['pass_retype'])) echo $err_msg['pass_retype']; ?></span>
-      <input type="text" name="email_retype" placeholder="パスワード（再入力）">
+      <input type="password" name="pass_retype" placeholder="パスワード（再入力）"value="<?php if(!empty($_POST['pass_retype'])) echo $_POST['pass_retype'];?>">
       <input type="submit" value="送信">
     </form>
     <a href="mypage.php">マイページへ</a>
